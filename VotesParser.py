3f3d0d5c8021055e5d4fa495467a37b70832856d
Vladislav Martynov <Wariorest@gmail.com>
2022-07-02 13:54:31 +0300

    fast commit

--- VotesParser.py
+++ VotesParser.py
@@ -4,21 +4,21 @@ from turtle import update
 import requests
 from bs4 import BeautifulSoup as BS
 
-def getVotesNumber(requestURL):
+def getVotesNumber(requestURL, htmlSelectClass = "", elSelectClass = ""):
     r = requests.get(requestURL)
     html = BS(r.content, 'html.parser')
-
-    for el in html.select(".petition_votes_txt"):
-        votes = el.select('.petition_votes_txt > span')
-
+    for el in html.select(htmlSelectClass):
+        votes = el.select(elSelectClass)
     return votes
 
 
-def processData(requestURL):
-    votesNumber = getVotesNumber(requestURL)
+def processData(requestURL, htmlSelectClass = "", elSelectClass = ""):
+    votesNumber = getVotesNumber(requestURL, htmlSelectClass, elSelectClass)
     return(votesNumber.pop().text)
 
 
 def votesPerHour(fVotesPoint, sVotesPoint):
     votesPerHour = fVotesPoint - sVotesPoint
     return votesPerHour
+
+
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Type
 from telegram.ext.updater import Updater
 from telegram.update import Update
 from telegram.ext.callbackcontext import CallbackContext
@@ -9,15 +10,30 @@ import VotesParser
 
 
 
-
-def start(update: Update, context: CallbackContext):
-    votesStr = VotesParser.processData("https://petition.president.gov.ua/petition/146960")
+def votes(update: Update, context: CallbackContext):
+    votesStr = VotesParser.processData("https://petition.president.gov.ua/petition/146960",
+    ".petition_votes_txt",
+    ".petition_votes_txt > span"
+    )
     mens = int(votesStr)
-    votesStr = VotesParser.processData("https://petition.president.gov.ua/petition/140908")
+    votesStr = VotesParser.processData("https://petition.president.gov.ua/petition/140908",
+    ".petition_votes_txt",
+    ".petition_votes_txt > span"
+    )
     students = int(votesStr)
     massage = "votes now: \n" + "age 18 - 60: " + str(mens) + "\nstudents: " + str(students)
-    update.message.reply_text(massage)    
-    
+    update.message.reply_text(massage)
+
+def pagePetitionStat(update: Update, context: CallbackContext):
+    votesStr = VotesParser.processData("https://petition.president.gov.ua/?status=active&sort=votes&order=desc",
+    ".pet_title",
+    ".pet_link"
+    )
+    print(Type(votesStr))
+  
+    #students = int(votesStr)
+    #massage = "votes now: \n" + "age 18 - 60: " + str(mens) + "\nstudents: " + str(students)
+    #update.message.reply_text(massage)
     
 
 
@@ -27,6 +43,7 @@ updater = Updater("5367058482:AAHB6YoooV46zM753Ng9UVJG3EyJpdTVT90",
                   
 
 
-updater.dispatcher.add_handler(CommandHandler('votes', start))
+updater.dispatcher.add_handler(CommandHandler('votes', votes))
+updater.dispatcher.add_handler(CommandHandler('stat', pagePetitionStat))
 
 updater.start_polling()
